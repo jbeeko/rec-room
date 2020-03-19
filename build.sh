89dad2150a891c14bdcb4f4cf70e1f2936783afa
Joerg Beekmann <joerg@purepaces.com>
2020-03-19 10:22:09 -0700

    Bump version to 0.0.1

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ set nuget-key=oy2gbbyumnd3vgf2j2wqshhuuspstb3evb2bhh3waluzey
 set github-user=jbeeko
 set github-pw=1MixedMess..
 
-dotnet fake build --target Release
+dotnet fake -v build --target Release
